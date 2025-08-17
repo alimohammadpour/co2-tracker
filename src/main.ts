08278fc5d48d9c75f32147ff696634ae294fd012
alimohammadpour <alimpr027@gmail.com>
2025-08-17 05:40:35 +0200

    adds swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('CO2-tracker APIs')
+    .setDescription('API documentation for CO2-tracker')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, documentFactory, { useGlobalPrefix: true });
 
   await app.listen(process.env.PORT ?? 3000);
 }
